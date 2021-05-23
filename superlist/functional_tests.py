90ba15d8aaf37df4d555672d4eaf845f9d318522
miktuy <miktuy@mail.ru>
2021-05-23 22:55:12 +0300

    Improved home page template

--- superlist/functional_tests.py
+++ superlist/functional_tests.py
@@ -34,7 +34,8 @@ class NewVisitorTest(unittest.TestCase):
         table = self.browser.find_element_by_id('id-list-table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
-            any(row.text == '1: Buy ostrich plum' for row in rows)
+            any(row.text == '1: Buy ostrich plum' for row in rows),
+            msg='New item is not found in table!'
         )
         # self.fail('Stop test!')
 
