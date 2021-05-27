fd442275a0a7c00abf14144afc26dd1bd2f417b8
miktuy <miktuy@mail.ru>
2021-05-27 09:54:22 +0300

    Extracted test step

--- superlist/functional_tests.py
+++ superlist/functional_tests.py
@@ -31,21 +31,22 @@ class NewVisitorTest(unittest.TestCase):
         input_box.send_keys(Keys.ENTER)
         time.sleep(1)  # ToDo: REMOVE THIS SHIT IN FUTURE!!!
 
-        table = self.browser.find_element_by_id('id-list-table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy ostrich plum', [row.text for row in rows])
+        self.list_table_should_contain_row('1: Buy ostrich plum')
 
         input_box = self.browser.find_element_by_id('id-new-item')
         input_box.send_keys('Produce bead from ostrich plum')
         input_box.send_keys(Keys.ENTER)
         time.sleep(1)  # ToDo: REMOVE THIS SHIT IN FUTURE!!!
 
-        table = self.browser.find_element_by_id('id-list-table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy ostrich plum', [row.text for row in rows])
-        self.assertIn('2: Produce bead from ostrich plum', [row.text for row in rows])
+        self.list_table_should_contain_row('1: Buy ostrich plum')
+        self.list_table_should_contain_row('2: Produce bead from ostrich plum')
 
         # self.fail('Stop test!')
+
+    def list_table_should_contain_row(self, row_text: str):
+        table = self.browser.find_element_by_id('id-list-table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
 
 
 if __name__ == "__main__":
