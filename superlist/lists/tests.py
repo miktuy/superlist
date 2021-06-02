f022f2dd64974e6f6a0ac9f3869b697bd1fbf967
miktuy <miktuy@mail.ru>
2021-06-02 22:38:08 +0300

    Added redirection after POST; shows elements of list in template

--- superlist/lists/tests.py
+++ superlist/lists/tests.py
@@ -10,16 +10,35 @@ class HomePageTest(TestCase):
         response: HttpResponse = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
 
+    def test_only_saves_items_when_necessary(self):
+        self.client.get('/')
+        self.assertEqual(Item.objects.count(), 0)
+
     def test_can_save_post_request(self):
-        response = self.client.post(
+        self.client.post(
             '/',
             data={'item_text': 'A new list item'},
         )
-        self.assertIn(
-            'A new list item',
-            response.content.decode()
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item: Item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+
+    def test_redirects_after_post(self):
+        response: HttpResponse = self.client.post(
+            '/',
+            data={'item_text': 'A new list item'},
         )
-        self.assertTemplateUsed(response, 'home.html')
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
+
+    def test_dispalays_all_list_items(self):
+        Item.objects.create(text='item 1')
+        Item.objects.create(text='item 2')
+
+        response: HttpResponse = self.client.get('/')
+        self.assertIn('item 1', response.content.decode())
+        self.assertIn('item 2', response.content.decode())
 
 
 class ItemModelTest(TestCase):
--- superlist/lists/views.py
+++ superlist/lists/views.py
@@ -1,11 +1,19 @@
+from typing import List
+
 from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from lists.models import Item
 
 
 def home_page(request: HttpRequest):
+    if request.method == 'POST':
+        Item.objects.create(text=request.POST['item_text'])
+        return redirect('/')
+    items: List[Item] = Item.objects.all()
     return render(
         request, 'home.html',
         {
-            'new_item_text': request.POST.get('item_text', ''),
+            'items': items,
         }
     )
