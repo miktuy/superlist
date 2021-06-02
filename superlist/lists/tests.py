e0ae905c0aa0c3c672e80a998030ee0ff490457c
miktuy <miktuy@mail.ru>
2021-06-02 17:07:29 +0300

    Added model `Item` and migration for this

--- superlist/lists/tests.py
+++ superlist/lists/tests.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from django.test import TestCase
 
+from lists.models import Item
+
 
 class HomePageTest(TestCase):
 
@@ -18,3 +20,22 @@ class HomePageTest(TestCase):
             response.content.decode()
         )
         self.assertTemplateUsed(response, 'home.html')
+
+
+class ItemModelTest(TestCase):
+    def test_saving_and_retreiving_items(self):
+        first_item: Item = Item()
+        first_item.text = 'The first (ever) list item'
+        first_item.save()
+
+        second_item: Item = Item()
+        second_item.text = 'Item the second'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(second_saved_item.text, 'Item the second')
