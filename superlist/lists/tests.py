78ad604592381d92f34a16ade05520c37f4f08bd
miktuy <miktuy@mail.ru>
2021-06-05 22:22:18 +0300

    new items connected with different lists

--- superlist/lists/tests.py
+++ superlist/lists/tests.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 from django.test import TestCase
 
-from lists.models import Item
+from lists.models import Item, List
 
 
 class HomePageTest(TestCase):
@@ -11,15 +11,23 @@ class HomePageTest(TestCase):
         self.assertTemplateUsed(response, 'home.html')
 
 
-class ItemModelTest(TestCase):
+class ListAndItemModelTest(TestCase):
     def test_saving_and_retreiving_items(self):
+        list_ = List()
+        list_.save()
+
         first_item: Item = Item()
         first_item.text = 'The first (ever) list item'
+        first_item.list = list_
         first_item.save()
 
         second_item: Item = Item()
         second_item.text = 'Item the second'
+        second_item.list = list_
         second_item.save()
+
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, list_)
 
         saved_items = Item.objects.all()
         self.assertEqual(saved_items.count(), 2)
@@ -27,7 +35,9 @@ class ItemModelTest(TestCase):
         first_saved_item = saved_items[0]
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(first_saved_item.list, list_)
         self.assertEqual(second_saved_item.text, 'Item the second')
+        self.assertEqual(second_saved_item.list, list_)
 
 
 class ListViewTest(TestCase):
@@ -37,8 +47,9 @@ class ListViewTest(TestCase):
         self.assertTemplateUsed(response, 'list.html')
 
     def test_dispalays_all_list_items(self):
-        Item.objects.create(text='item 1')
-        Item.objects.create(text='item 2')
+        list_ = List.objects.create()
+        Item.objects.create(text='item 1', list=list_)
+        Item.objects.create(text='item 2', list=list_)
 
         response: HttpResponse = self.client.get('/lists/unique-list')
         self.assertContains(response, 'item 1')
--- superlist/lists/views.py
+++ superlist/lists/views.py
@@ -1,9 +1,9 @@
-from typing import List
+from typing import List as TList
 
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render, redirect
 
-from lists.models import Item
+from lists.models import Item, List
 
 
 def home_page(request: HttpRequest):
@@ -11,10 +11,11 @@ def home_page(request: HttpRequest):
 
 
 def view_list(request: HttpRequest):
-    items: List[Item] = Item.objects.all()
+    items: TList[Item] = Item.objects.all()
     return render(request, "list.html", {"items": items})
 
 
 def new_list(request: HttpRequest):
-    Item.objects.create(text=request.POST["item_text"])
+    list_ = List.objects.create()
+    Item.objects.create(text=request.POST["item_text"], list=list_)
     return redirect("/lists/unique-list")
