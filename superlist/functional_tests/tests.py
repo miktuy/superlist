40340fd691448b2abc0cfc85f357c84e9410889d
miktuy <miktuy@mail.ru>
2021-06-07 20:49:29 +0300

    Added stupid FT for page style formatting

--- superlist/functional_tests/tests.py
+++ superlist/functional_tests/tests.py
@@ -27,6 +27,27 @@ class NewVisitorTest(LiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def test_layout_and_styling(self):
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        input_box = self.browser.find_element_by_id("id-new-item")
+        self.assertAlmostEqual(
+            input_box.location['x'] + input_box.size['width'] / 2,
+            512,
+            delta=10
+        )
+
+        input_box.send_keys("testing")
+        input_box.send_keys(Keys.ENTER)
+        self.list_table_should_contain_row("1: testing")
+        input_box = self.browser.find_element_by_id("id-new-item")
+        self.assertAlmostEqual(
+            input_box.location['x'] + input_box.size['width'] / 2,
+            512,
+            delta=10
+        )
+
     def test_can_start_a_list_and_retrieve_in_later(self):
         self.browser.get(self.live_server_url)
         self.assertIn("To-Do", self.browser.title)
