21543b61f542aceca534c6dc2c2b2d1ac6f552d8
miktuy <miktuy@mail.ru>
2021-07-30 00:02:53 +0300

    fixed paths

--- superlist/settings.py
+++ superlist/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'superlist.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / '../database/db.sqlite3',
+        'NAME': BASE_DIR / '/../../database/db.sqlite3',
     }
 }
 
